012455b6bb7f836f6e6060e587c2acffaa0a311c
nitin1326 <kapsikarpk@gmail.com>
2025-04-05 16:08:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-const countrycontainers = document.querySelector('.countries-container')
+const countrycontainers = document.querySelector('.countries-container');
 
 fetch("https://restcountries.com/v3.1/all")
-.then((res)=> res.json())
-   .then((data)=>{
-    data.forEach((country)=>{
-        const countryCard = document.createElement('a');
+  .then((res) => res.json())
+  .then((data) => {
+    let foundIndia = false;  // To check if India appears
+    data.forEach((country) => {
+      const countryCard = document.createElement('a');
+      countryCard.classList.add('country-card');
 
-        countryCard.classList.add('country-card')
-        
-        const cardHtml = ` 
-         <img src="${country.flags.svg}" alt="flag">
-                        <div class="card-text">
-                            <h3 class="card-title">${country.name.common}</h3>
-                            <p><b>Population: </b>${country.population.toLocaleString('en-In')}</p>
-                            <p><b>Region: </b>${country.region}</p>
-                            <p><b>Capital: </b>${country.capital[0]}</p>
-                        </div>
-        `
-        
-        countryCard.innerHTML = cardHtml
-        countryCard.href = "/details.html"
-        
-        countrycontainers.append(countryCard)
-    })
-})
+      // Check if capital exists
+      const capital = country.capital ? country.capital[0] : "N/A";
 
+      const cardHtml = ` 
+        <img src="${country.flags.svg}" alt="flag">
+        <div class="card-text">
+          <h3 class="card-title">${country.name.common}</h3>
+          <p><b>Population: </b>${country.population.toLocaleString('en-IN')}</p>
+          <p><b>Region: </b>${country.region}</p>
+          <p><b>Capital: </b>${capital}</p>
+        </div>
+      `;
 
+      countryCard.innerHTML = cardHtml;
+      countryCard.href = `/details.html?name=${encodeURIComponent(country.name.common)}`;
 
+      countrycontainers.append(countryCard);
 
+      if (country.name.common === "India") {
+        foundIndia = true;
+      }
+    });
 
+    if (!foundIndia) {
+      console.warn("India is missing from the API response!");
+    }
+  })
+  .catch((error) => console.error("Error fetching country data:", error));
--- script2.js
+++ script2.js
@@ -1,4 +1,71 @@
 const back = document.querySelector('.back-button');
+back.href = "./index.html";
 
+const countryName = new URLSearchParams(location.search).get('name');
 
-    back.href = "./index.html"
+const flagImage = document.querySelector('.country-img');
+const countryTitle = document.querySelector('.grid-data h1');
+const natviveName = document.querySelector('.native-name');
+const region_data = document.querySelector('.region');
+const sub_region = document.querySelector('.subregion');
+const capital = document.querySelector('.capital');
+const level_domain = document.querySelector('.top-level-domain');
+const currence = document.querySelector('.currence');
+const language = document.querySelector('.language');
+const bordersDiv = document.getElementById("borders");
+
+fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
+  .then(res => res.json())
+  .then(([data]) => {
+    // Flag & name
+    flagImage.src = data.flags.svg;
+    countryTitle.innerText = data.name.common;
+
+    // Native name
+    if (data.name.nativeName) {
+      natviveName.innerText = Object.values(data.name.nativeName)[0].common;
+    }
+
+    // Region info
+    region_data.innerText = data.region;
+    sub_region.innerText = data.subregion;
+    capital.innerText = data.capital;
+    level_domain.innerText = data.tld;
+
+    // Currency
+    const currencyCode = Object.keys(data.currencies)[0];
+    currence.innerText = data.currencies[currencyCode].symbol;
+
+    // Languages
+    const languageKeys = Object.keys(data.languages);
+    const firstTwoLanguages = languageKeys.slice(0, 2);
+    language.innerText = firstTwoLanguages.map(key => data.languages[key]).join(", ");
+
+    // Borders
+    bordersDiv.innerHTML = "<h2>Border Countries:</h2>";
+    const borderCodes = data.borders;
+
+    if (!borderCodes || borderCodes.length === 0) {
+      bordersDiv.innerHTML += "<p>No border countries</p>";
+    } else {
+      fetch(`https://restcountries.com/v3.1/alpha?codes=${borderCodes.join(",")}`)
+        .then(res => res.json())
+        .then(borderCountries => {
+          borderCountries.forEach(border => {
+            const p = document.createElement("p");
+            p.innerText = border.name.common;
+            p.style.display = "inline-block";   // Make sure it's visible
+            p.style.marginRight = "10px";
+            p.style.padding = "5px 10px";
+            p.style.border = "1px solid #ccc";
+            p.style.borderRadius = "5px";
+            p.style.backgroundColor = "#f0f0f0";
+            bordersDiv.appendChild(p);
+          });
+        });
+    }
+  })
+  .catch(error => {
+    console.error("Error fetching country data:", error);
+    bordersDiv.innerHTML = "<h2>Border Countries:</h2><p>Error loading borders</p>";
+  });
